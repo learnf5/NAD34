51280a06b15fda29fd0b66735f7e7806ac11ff47
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-05-15 06:58:15 -0700

    Update 20-NAD34-L07L01-PRXYREQ.sh

--- 20-NAD34-L07L01-PRXYREQ.sh
+++ 20-NAD34-L07L01-PRXYREQ.sh
@@ -1,5 +1,5 @@
 # Do we need this file? In nad32 we have it but in nag it isn't in script
-# curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab06/variable_test.bak
+# curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/NAD34/main/lab06/variable_test.bak
 # sudo scp /tmp/variable_test.bak nginx:/etc/nginx/conf.d/
 
 # enable debugging
@@ -8,7 +8,7 @@ PS4='+$(date +"%T.%3N"): '
 
 # update Student Workstation host for the specific lab
 # only if need a simpler /etc/hosts file do the following 2 steps
-# curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab01/hosts
+# curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/NAD34/main/lab01/hosts
 # sudo cp /tmp/hosts                                  /etc/hosts
 
 # update nginx host for the specific lab
@@ -16,11 +16,14 @@ PS4='+$(date +"%T.%3N"): '
 #Remove default.conf file since this lab requires only default.bak
 sudo ssh nginx rm /etc/nginx/conf.d/default.conf
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab03/default.bak
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/NAD34/main/lab03/default.bak
 sudo scp /tmp/default.bak                              nginx:/etc/nginx/conf.d/
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab07/mywebserver.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/NAD34/main/lab07/mywebserver.conf
 sudo scp /tmp/mywebserver.conf                              nginx:/etc/nginx/conf.d/
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab03/return_test.bak
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/NAD34/main/lab03/return_test.bak
 sudo scp /tmp/return_test.bak      
+
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/NAD34/main/lab06/variable_test.bak
+sudo scp /tmp/variable_test.bak                              nginx:/etc/nginx/conf.d/
