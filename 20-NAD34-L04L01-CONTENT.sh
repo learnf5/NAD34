5b66f63c79c28bda0b16371511df365ad8928882
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-05-15 07:19:24 -0700

    Update 20-NAD34-L04L01-CONTENT.sh

--- 20-NAD34-L04L01-CONTENT.sh
+++ 20-NAD34-L04L01-CONTENT.sh
@@ -5,14 +5,14 @@ PS4='+$(date +"%T.%3N"): '
 #Remove default.conf file since this lab requires only default.bak
 sudo ssh nginx rm /etc/nginx/conf.d/default.conf
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/NAD34/main/lab03/default.bak
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/lab03/default.bak
 sudo scp /tmp/default.bak                              nginx:/etc/nginx/conf.d/
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/NAD34/main/lab03/mywebserver.bak
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/lab03/mywebserver.bak
 sudo scp /tmp/mywebserver.bak                              nginx:/etc/nginx/conf.d/
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/NAD34/main/lab03/return_test.bak
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/lab03/return_test.bak
 sudo scp /tmp/return_test.bak                              nginx:/etc/nginx/conf.d/
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/NAD34/main/lab04/Lab4_index.html
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/lab04/Lab4_index.html
 sudo scp /tmp/Lab4_index.html                              nginx:/home/ubuntu/public_html/index.html
