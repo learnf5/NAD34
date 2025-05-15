f7b6db185679869d1b6df34569c7e3b04af9b87b
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-05-15 06:52:35 -0700

    Update 20-NAD34-L07L01-PRXYREQ.sh

--- 20-NAD34-L07L01-PRXYREQ.sh
+++ 20-NAD34-L07L01-PRXYREQ.sh
@@ -1,14 +1,26 @@
-# Remove default.conf file since this lab requires only default.bak
+# Do we need this file? In nad32 we have it but in nag it isn't in script
+# curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab06/variable_test.bak
+# sudo scp /tmp/variable_test.bak nginx:/etc/nginx/conf.d/
+
+# enable debugging
+set -x
+PS4='+$(date +"%T.%3N"): '
+
+# update Student Workstation host for the specific lab
+# only if need a simpler /etc/hosts file do the following 2 steps
+# curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab01/hosts
+# sudo cp /tmp/hosts                                  /etc/hosts
+
+# update nginx host for the specific lab
+
+#Remove default.conf file since this lab requires only default.bak
 sudo ssh nginx rm /etc/nginx/conf.d/default.conf
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab03/default.bak
-sudo scp /tmp/default.bak       nginx:/etc/nginx/conf.d/
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab03/default.bak
+sudo scp /tmp/default.bak                              nginx:/etc/nginx/conf.d/
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab07/mywebserver.conf
-sudo scp /tmp/mywebserver.conf  nginx:/etc/nginx/conf.d/
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab07/mywebserver.conf
+sudo scp /tmp/mywebserver.conf                              nginx:/etc/nginx/conf.d/
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab03/return_test.bak
-sudo scp /tmp/return_test.bak   nginx:/etc/nginx/conf.d/
-
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab06/variable_test.bak
-sudo scp /tmp/variable_test.bak nginx:/etc/nginx/conf.d/
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab03/return_test.bak
+sudo scp /tmp/return_test.bak      
