0bd9c17e92d4581c3028e5d2386183e5bc17d4bf
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-05-15 07:20:53 -0700

    Update 20-NAD34-L07L01-PRXYREQ.sh

--- 20-NAD34-L07L01-PRXYREQ.sh
+++ 20-NAD34-L07L01-PRXYREQ.sh
@@ -8,7 +8,7 @@ PS4='+$(date +"%T.%3N"): '
 
 # update Student Workstation host for the specific lab
 # only if need a simpler /etc/hosts file do the following 2 steps
-# curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/NAD34/main/lab01/hosts
+# curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/lab01/hosts
 # sudo cp /tmp/hosts                                  /etc/hosts
 
 # update nginx host for the specific lab
@@ -16,14 +16,14 @@ PS4='+$(date +"%T.%3N"): '
 #Remove default.conf file since this lab requires only default.bak
 sudo ssh nginx rm /etc/nginx/conf.d/default.conf
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/NAD34/main/lab03/default.bak
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/lab03/default.bak
 sudo scp /tmp/default.bak                              nginx:/etc/nginx/conf.d/
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/NAD34/main/lab07/mywebserver.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/lab07/mywebserver.conf
 sudo scp /tmp/mywebserver.conf                              nginx:/etc/nginx/conf.d/
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/NAD34/main/lab03/return_test.bak
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/lab03/return_test.bak
 sudo scp /tmp/return_test.bak      
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/NAD34/main/lab06/variable_test.bak
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/lab06/variable_test.bak
 sudo scp /tmp/variable_test.bak                              nginx:/etc/nginx/conf.d/
