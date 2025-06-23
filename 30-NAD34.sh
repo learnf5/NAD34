c767d721250c47b1334489c6774f24a0116094b5
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-06-23 14:23:34 -0700

    Update 30-NAD34.sh

--- 30-NAD34.sh
+++ 30-NAD34.sh
@@ -1,5 +1,10 @@
+# Sorry for my mistake (Vicki June 21, 2025) - I left nap.conf and nap.orig on the NGINX base VM
+# So I'll remove it for all NGINX labs from this script
+sudo ssh nginx sudo rm /etc/nginx/conf.d/nap.{conf,orig}
+
 # Install tree command on NGINX server
 sudo ssh nginx sudo apt install tree
+
 # restart NGINX
 sudo ssh nginx sudo systemctl stop nginx
 sudo ssh nginx sudo systemctl start nginx
