d7f3a0a9178c6d513d2761672c3bb2002ebebd84
djlamey <76072289+djlamey@users.noreply.github.com>
2025-06-27 14:57:26 -0400

    Update 20-NAD34-L01L01-PROCESS.sh

--- 20-NAD34-L01L01-PROCESS.sh
+++ 20-NAD34-L01L01-PROCESS.sh
@@ -2,4 +2,6 @@
 set -x
 PS4='+$(date +"%T.%3N"): '
 
-# No custom files required for this lab
+# Custom files required for this lab
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/lab01/default.conf
+sudo scp /tmp/default.conf                                   nginx:/etc/nginx/conf.d/default.conf
